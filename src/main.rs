6d4f57392b182b331bdd768fdc657b484661a295
Sept <sszheng3@gmail.com>
2023-02-21 23:34:46 -0500

    fixed the Dockerfile

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,3 @@
-// use clap::Parser;
-//
-// mod lib;
-// mod tests;
-//
-//
-// #[derive(Parser)]
-// #[clap(
-// version = "1.0",
-// author = "Shuai Zheng",
-// about = "Command-line interface for Summarization"
-// )]
-// struct Cli {
-//     #[clap(subcommand)]
-//     command: Option<Commands>,
-// }
-//
-// #[derive(Parser)]
-// enum Commands {
-//     #[clap(version = "1.0", author = "Shuai Zheng")]
-//     Text {
-//         #[clap(short, long)]
-//         input: String,
-//     },
-// }
-//
-// fn main() {
-//     let args = Cli::parse();
-//     match args.command {
-//         Some(Commands::Text { input }) => {
-//             let summarization_model = lib::init_summarization_model();
-//
-//             let mut inputs = [String::new(); 1];
-//             inputs[0] = input.to_owned();
-//
-//             let _output = summarization_model.summarize(&inputs);
-//             let mut result = String::from(_output.join(" "));
-//             println!("Below is the summary:");
-//
-//             println!("{:#?}", result);
-//         }
-//         None => {
-//             println!("No command given");
-//         }
-//     }
-// }
 mod lib;
 mod tests;
 use actix_web::middleware::Logger;
@@ -120,13 +74,6 @@ async fn main() -> Result<(), ExitFailure> {
         std::env::set_var("RUST_LOG", "actix_web=info");
     }
     env_logger::init();
-
-    // let mut rt = Runtime::new().unwrap();
-    // rt.block_on(async {
-    //     init_summarization_model();
-    //     drop(rt); // this line causes the error
-    // });
-
 
 
     println!("Server started successfully");
