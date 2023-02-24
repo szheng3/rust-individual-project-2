f5a33d2aadc1cb81b40b67f4270add296a7b274b
Sept <sszheng3@gmail.com>
2023-02-24 02:24:46 -0500

    git

--- frontend-summarization/src/views/SummarizationView.vue
+++ frontend-summarization/src/views/SummarizationView.vue
@@ -1,94 +1,123 @@
 <template>
   <v-app>
-    <v-container>
-      <v-row>
-        <v-col cols="12"
-               sm="6">
-          <v-form @submit.prevent="submitForm">
+    <v-app-bar flat>
+      <v-container class="fill-height d-flex align-center">
+<!--        <v-avatar-->
+<!--            class="me-10 ms-4"-->
+<!--            color="grey-darken-1"-->
+<!--            size="32"-->
+<!--        ></v-avatar>-->
+
+        <v-btn
+            v-for="link in links"
+            :key="link"
+            variant="text"
+        >
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+      </v-container>
+    </v-app-bar>
+    <v-main class="bg-grey-lighten-4">
+
+      <v-container>
+        <v-row>
+          <v-col cols="12"
+                 sm="6">
+            <v-sheet rounded class="mt-4">
+
+              <v-form @submit.prevent="submitForm" class="pa-3">
 
 
-            <v-textarea
-                rounded
-                v-model="textInput"
-                label="Enter text to summarize"
-                rows="20"
-                class="ma-lg-0"
-                :loading="loading"></v-textarea>
-            <div class="text-caption">
-              Longer text will take longer to process. Output words about:
-              <strong>{{ Math.round(sliderValue / 100 * textInput.split(' ').length) }}</strong>
-            </div>
-            <v-slider v-model="sliderValue" min="0" max="100"
-                      :thumb-size="15"
+                <v-textarea
+                    rounded
+                    v-model="textInput"
+                    label="Enter text to summarize"
+                    rows="23"
+                    :loading="loading"></v-textarea>
 
-                      thumb-label=true></v-slider>
+                <div class="text-caption">
+                  Longer text will take longer to process. Output words about:
+                  <strong>{{ Math.round(sliderValue / 100 * textInput.split(' ').length) }}</strong>
+                </div>
+                <v-slider v-model="sliderValue" min="0" max="100"
+                          :thumb-size="15"
+
+                          thumb-label=true></v-slider>
 
 
-            <v-btn :loading="loading" type="submit" color="primary">Submit</v-btn>
-          </v-form>
-        </v-col>
-        <v-col cols="12"
-               sm="6">
-          <v-card v-if="showResult" variant="tonal">
-            <v-card-title class="headline">{{ result.status }}</v-card-title>
-            <v-card-text>{{ result.message }}</v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-divider class="mt-10"></v-divider>
+                <v-btn :loading="loading" type="submit" color="primary">Submit</v-btn>
+              </v-form>
+            </v-sheet>
 
-      <v-footer
-          class=" text-center d-flex flex-column mt-5"
-      >
+          </v-col>
 
-        <div class="text-caption">
-          This project aims to build a Rust CLI tool that summarizes text, based on the common task of reading and summarizing books among students.
-        </div>
+          <v-col cols="12"
+                 sm="6">
 
-        <v-divider></v-divider>
+            <v-card v-if="showResult" variant="flat" class="mt-4">
+              <v-card-title class="headline">{{ result.status }}</v-card-title>
+              <v-card-text>{{ result.message }}</v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
 
-        <div class="text-caption">
-          {{ new Date().getFullYear() }} —Shuai Zheng
-        </div>
-      </v-footer>
-    </v-container>
+
+      </v-container>
+    </v-main>
+    <v-footer
+        class=" text-center d-flex flex-column"
+        flat
+    >
+
+      <div class="text-caption">
+        This project aims to build a Rust CLI tool that summarizes text, based on the common task of reading and
+        summarizing books among students.
+      </div>
+
+
+      <div class="text-caption">
+        {{ new Date().getFullYear() }} —Shuai Zheng
+      </div>
+    </v-footer>
   </v-app>
 </template>
 
 
-<script>
+<script setup>
 import {ref} from 'vue';
 import axios from "axios";
 
-export default {
-  setup() {
-    const textInput = ref('');
-    const loading = ref(false);
-    const showResult = ref(false);
-    const sliderValue = ref(50);
-    const result = ref({status: 'success', message: 'Sample'});
 
-    const submitForm = async () => {
-      loading.value = true;
-      try {
+const textInput = ref('');
+const loading = ref(false);
+const showResult = ref(false);
+const sliderValue = ref(50);
+const result = ref({status: 'success', message: 'Sample'});
+const links = ref([
+  'Summarization',
+]);
 
-        const response = await axios.post('/api/summary', {
-          context: textInput.value,
-          minlength: Math.round(sliderValue.value / 100 * textInput.value.split(' ').length)
-        });
-        result.value = await response.data;
-        showResult.value = true;
+const submitForm = async () => {
+  loading.value = true;
+  try {
+
+    const response = await axios.post('/api/summary', {
+      context: textInput.value,
+      minlength: Math.round(sliderValue.value / 100 * textInput.value.split(' ').length)
+    });
+    result.value = await response.data;
+    showResult.value = true;
 
 
-      } catch (error) {
-        console.error(error);
-      } finally {
-        loading.value = false;
-      }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    loading.value = false;
+  }
 
-    };
 
-    return {textInput, loading, showResult, result, submitForm, sliderValue};
-  },
 };
 </script>
